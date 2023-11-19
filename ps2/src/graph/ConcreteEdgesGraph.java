7ae7c093c19f7b13423d51965974d17375287beb
Muhammad Bin Usman <muhammadbin.2003@gmail.com>
2023-11-19 16:53:03 +0500

    Implemented ConcreteEdgesGraph.java

--- ps2/src/graph/ConcreteEdgesGraph.java
+++ ps2/src/graph/ConcreteEdgesGraph.java
@@ -4,7 +4,9 @@
 package graph;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -12,50 +14,115 @@ import java.util.Set;
 /**
  * An implementation of Graph.
  * 
- * <p>PS2 instructions: you MUST use the provided rep.
+ * <p>
+ * PS2 instructions: you MUST use the provided rep.
  */
+
+// ConcreteEdgesGraph class
+
+// ConcreteEdgesGraph class
 public class ConcreteEdgesGraph implements Graph<String> {
-    
+
     private final Set<String> vertices = new HashSet<>();
     private final List<Edge> edges = new ArrayList<>();
-    
+
     // Abstraction function:
-    //   TODO
+    // Represents a weighted directed graph with labeled vertices and edges.
     // Representation invariant:
-    //   TODO
+    // Edges in the 'edges' list must connect vertices in 'vertices'.
     // Safety from rep exposure:
-    //   TODO
-    
-    // TODO constructor
-    
-    // TODO checkRep
-    
-    @Override public boolean add(String vertex) {
-        throw new RuntimeException("not implemented");
+    // All fields are private. Defensive copies are used where needed.
+
+    // Constructor
+    public ConcreteEdgesGraph() {
+        // Initialize the graph if needed
     }
-    
-    @Override public int set(String source, String target, int weight) {
-        throw new RuntimeException("not implemented");
+
+    // Representation invariant check
+    private void checkRep() {
+        for (Edge edge : edges) {
+            assert vertices.contains(edge.getSource()) : "Source vertex not in vertices set";
+            assert vertices.contains(edge.getTarget()) : "Target vertex not in vertices set";
+        }
     }
-    
-    @Override public boolean remove(String vertex) {
-        throw new RuntimeException("not implemented");
+
+    @Override
+    public boolean add(String vertex) {
+        if (vertices.add(vertex)) {
+            checkRep();
+            return true;
+        }
+        return false;
     }
-    
-    @Override public Set<String> vertices() {
-        throw new RuntimeException("not implemented");
+
+    @Override
+    public int set(String source, String target, int weight) {
+        Iterator<Edge> iterator = edges.iterator();
+        while (iterator.hasNext()) {
+            Edge edge = iterator.next();
+            if (edge.getSource().equals(source) && edge.getTarget().equals(target)) {
+                int previousWeight = edge.getWeight();
+                iterator.remove(); // Use iterator to safely remove the element
+                edges.add(new Edge(source, target, weight));
+                vertices.add(source);
+                vertices.add(target);
+                checkRep(); // Call checkRep after modification
+                return previousWeight;
+            }
+        }
+        edges.add(new Edge(source, target, weight));
+        vertices.add(source);
+        vertices.add(target);
+        checkRep(); // Call checkRep after modification
+        return 0;
     }
-    
-    @Override public Map<String, Integer> sources(String target) {
-        throw new RuntimeException("not implemented");
+
+    @Override
+    public boolean remove(String vertex) {
+        if (vertices.remove(vertex)) {
+            edges.removeIf(edge -> edge.getSource().equals(vertex) || edge.getTarget().equals(vertex));
+            checkRep(); // Call checkRep after modification
+            return true;
+        }
+        return false;
     }
-    
-    @Override public Map<String, Integer> targets(String source) {
-        throw new RuntimeException("not implemented");
+
+    @Override
+    public Set<String> vertices() {
+        return new HashSet<>(vertices);
     }
-    
-    // TODO toString()
-    
+
+    @Override
+    public Map<String, Integer> sources(String target) {
+        Map<String, Integer> sourceMap = new HashMap<>();
+        for (Edge edge : edges) {
+            if (edge.getTarget().equals(target)) {
+                sourceMap.put(edge.getSource(), edge.getWeight());
+            }
+        }
+        return sourceMap;
+    }
+
+    @Override
+    public Map<String, Integer> targets(String source) {
+        Map<String, Integer> targetMap = new HashMap<>();
+        for (Edge edge : edges) {
+            if (edge.getSource().equals(source)) {
+                targetMap.put(edge.getTarget(), edge.getWeight());
+            }
+        }
+        return targetMap;
+    }
+
+    // toString()
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        for (Edge edge : edges) {
+            result.append(edge.toString()).append("\n");
+        }
+        return result.toString();
+    }
 }
 
 /**
@@ -63,26 +130,77 @@ public class ConcreteEdgesGraph implements Graph<String> {
  * Immutable.
  * This class is internal to the rep of ConcreteEdgesGraph.
  * 
- * <p>PS2 instructions: the specification and implementation of this class is
+ * <p>
+ * PS2 instructions: the specification and implementation of this class is
  * up to you.
  */
 class Edge {
-    
     // TODO fields
-    
+    private final String source;
+    private final String target;
+    private final int weight;
+
     // Abstraction function:
-    //   TODO
+    // Represents a directed edge from source to target with a given weight.
     // Representation invariant:
-    //   TODO
+    // None (No checks are needed as this class is immutable).
     // Safety from rep exposure:
-    //   TODO
-    
+    // All fields are private and final.
+
     // TODO constructor
-    
-    // TODO checkRep
-    
+    // Constructor
+    public Edge(String source, String target, int weight) {
+        this.source = source;
+        this.target = target;
+        this.weight = weight;
+    }
+
+    // Observer methods
     // TODO methods
-    
-    // TODO toString()
-    
+    public String getSource() {
+        return source;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    // TODO toString() method
+    @Override
+    public String toString() {
+        return String.format("(%s -> %s, weight: %d)", source, target, weight);
+    }
+
+    // hashCode() method
+    @Override
+    public int hashCode() {
+        int result = source.hashCode();
+        result = 31 * result + target.hashCode();
+        result = 31 * result + weight;
+        return result;
+    }
+
+    // equals() method
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        Edge edge = (Edge) obj;
+        return weight == edge.weight &&
+                source.equals(edge.source) &&
+                target.equals(edge.target);
+    }
+
+    // TODO checkRep
+    // Check representation
+    private void checkRep() {
+        // No invariants to check in an immutable class
+    }
+
 }
