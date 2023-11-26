8345a00b6bbf2eaef8ffe6086f4f7933e93d51f0
Muhammad Bin Usman <muhammadbin.2003@gmail.com>
2023-11-26 11:28:31 +0500

    Test Cases for GraphPoet.java

--- ps2/test/poet/GraphPoetTest.java
+++ ps2/test/poet/GraphPoetTest.java
@@ -1,80 +1,143 @@
-/* Copyright (c) 2015-2016 MIT 6.005 course staff, all rights reserved.
- * Redistribution of original or derived work requires permission of course staff.
- */
+
 package poet;
 
-import static org.junit.Assert.*;
+
+
+import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Tests for GraphPoet.
  */
 public class GraphPoetTest {
-    
-    // Testing strategy for poem():
-    //      num bridge words - 0, 1, n
-    //      multiple bridges weight - same, different
-    //      case-sensitivity
-    //      multiple lines
-    
-    @Test(expected=AssertionError.class)
+
+    // Testing strategy
+    //   TODO
+
+    // TODO tests for GraphPoet
+
+    // TODO tests for checkRep
+
+    // TODO tests for poem
+
+    // TODO tests for safety from rep exposure
+    @Test(expected = AssertionError.class)
     public void testAssertionsEnabled() {
         assert false; // make sure assertions are enabled with VM argument: -ea
     }
-    
-    // tests
-    
-    // covers multiple lines
+
     @Test
-    public void testMultipleLines() throws IOException {
-        GraphPoet poet = new GraphPoet(new File("test/poet/Where-no-man-has-gone-before.txt"));
-        String poem = poet.poem("Seek to explore new and exciting synergies!");
-        
-        assertEquals("test multiple lines", poem, "Seek to explore strange new life and exciting synergies!");
+    public void testGraphPoetConstructor() throws IOException {
+        // Test the GraphPoet(File) constructor with a sample corpus file
+        GraphPoet samplePoet = new GraphPoet(new File("test/poet/sample-corpus.txt"));
+
+        // Add assertions to check the state of the GraphPoet object after
+        // construction
+        // For example, check that the graph is not empty
+        //assertTrue(!samplePoet.graph.toString().isEmpty());
     }
-    
-    // covers num bridge - 0
+
     @Test
-    public void testNoBridge() throws IOException {
-        GraphPoet poet = new GraphPoet(new File("test/poet/Where-no-man-has-gone-before.txt"));
-        String poem = poet.poem("no bridge");
-        
-        assertEquals("expected no bridge words",poem, "no bridge");
+    public void testPoemGeneration1() throws IOException {
+        GraphPoet samplePoet = new GraphPoet(new File("test/poet/sample-corpus.txt"));
+        // Test the poem(String) method with different input strings
+
+        String input1 = "Test of the mugar system.";
+        String expectedOutput1 = "Test of of the the mugar mugar omni system.";
+        assertEquals(expectedOutput1, samplePoet.poem(input1));
     }
-    
-    // covers num bridge - 1
     @Test
-    public void testOneBridge() throws IOException {
-        GraphPoet poet = new GraphPoet(new File("test/poet/one-bridge.txt"));
-        String poem = poet.poem("one bridge");
-        
-        assertEquals("expected one bridge word",poem, "one simple bridge");
+    public void testPoemGeneration2() throws IOException {
+        GraphPoet samplePoet = new GraphPoet(new File("test/poet/sample-corpus.txt"));
+        // Test case 2
+        String input2 = "This is a test.";
+        String expectedOutput2 = "This is is a a test test.";
+        assertEquals(expectedOutput2, samplePoet.poem(input2));
     }
-    
-    // covers num bridge - n
-    //        bridge - same weight
-    //        case insensitive
     @Test
-    public void testMultipleBridgesSameWeightCaseInsensitive() throws IOException {
-        GraphPoet poet = new GraphPoet(new File("test/poet/multiple-bridges-same-weight-case-insensitive.txt"));
-        String poem = poet.poem("MULTIPLE bridges");
-        
-        assertTrue("expected multiple bridge words with same weight", 
-                   poem.equals("MULTIPLE same bridges") || poem.equals("MULTIPLE weight bridges"));
+    public void testPoemGeneration3() throws IOException {
+        GraphPoet samplePoet = new GraphPoet(new File("test/poet/sample-corpus.txt"));
+        // Test case 3
+        String input3 = "One small step.";
+        String expectedOutput3 = "One small step.";
+        assertEquals(expectedOutput3, samplePoet.poem(input3));
     }
-    
-    // covers multiple bridges - different weight
     @Test
-    public void testMultipleBridgesDifferentWeight() throws IOException {
-        GraphPoet poet = new GraphPoet(new File("test/poet/multiple-bridges-different-weight.txt"));
-        String poem = poet.poem("multiple bridges");
-        
-        assertEquals("expected multiple bridge words woth different weight",
-                     poem, "multiple same bridges");
+    public void testPoemGeneration4() throws IOException {
+        GraphPoet samplePoet = new GraphPoet(new File("test/poet/sample-corpus.txt"));
+        // Test case 4
+        String input4 = "Graphs are interesting.";
+        String expectedOutput4 = "Graphs are interesting.";
+        assertEquals(expectedOutput4, samplePoet.poem(input4));
     }
-    
+
+    @Test
+    public void testPoemGeneration5() throws IOException {
+        GraphPoet samplePoet = new GraphPoet(new File("test/poet/sample-corpus.txt"));
+        // Test case 5
+        String input5 = "Empty input test.";
+        String expectedOutput5 = "Empty input test.";
+        assertEquals(expectedOutput5, samplePoet.poem(input5));
+    }
+    @Test
+    public void testPoemGenerationEmpty() throws IOException {
+        GraphPoet samplePoet = new GraphPoet(new File("test/poet/sample-corpus.txt"));
+        // Test case 6
+        String input6 = "";
+        String expectedOutput6 = "";
+        assertEquals(expectedOutput6, samplePoet.poem(input6));
+    }
+    @Test
+    public void testPoemGenerationRepeatedWords() throws IOException {
+        GraphPoet samplePoet = new GraphPoet(new File("test/poet/sample-corpus.txt"));
+        // Additional tests
+        // Test case 7: Test with repeated words in the input
+        String input7 = "Hello hello world world.";
+        String expectedOutput7 = "Hello hello world world.";
+        assertEquals(expectedOutput7, samplePoet.poem(input7));
+
+    }
+
+    @Test
+    public void testPoemGenerationSingleWord() throws IOException {
+        // Test case : Test with a corpus containing only one word
+        GraphPoet singleWordPoet = new GraphPoet(new File("test/poet/single-word-corpus.txt"));
+        String input8 = "Test the system.";
+        String expectedOutput8 = "Test the system.";
+        assertEquals(expectedOutput8, singleWordPoet.poem(input8));
+    }
+    @Test
+    public void testPoemGenerationTwoWord() throws IOException {
+        // Test case : Test with a corpus containing only two words
+        GraphPoet twoWordPoet = new GraphPoet(new File("test/poet/two-word-corpus"));
+        String input9 = "Test the system.";
+        String expectedOutput9 = "Test system\n" +
+                " the system.";
+        assertEquals(expectedOutput9, twoWordPoet.poem(input9).replace("\r",""));
+    }
+    @Test
+    public void testPoemGenerationMixedCase() throws IOException {
+        // Test case 10: Test with a corpus containing a mix of upper and lower case
+        GraphPoet mixedCasePoet = new GraphPoet(new File("test/poet/mixed-case-corpus"));
+        String input10 = "Mixed Case Test.";
+        String expectedOutput10 = "Mixed case Case test\n" +
+                " Test.";
+        assertEquals(expectedOutput10, mixedCasePoet.poem(input10).replace("\r",""));
+    }
+
+    @Test
+    public void testSafetyFromRepExposure() throws IOException {
+        // Test safety from rep exposure, if applicable
+        GraphPoet poet = new GraphPoet(new File("test/poet/sample-corpus.txt"));
+        // Add assertions to check that the representation is not exposed
+        // For example, try to modify the internal state of the object through rep
+        // exposure
+        // and ensure that it is not possible
+        // Make sure to respect encapsulation
+    }
 }
